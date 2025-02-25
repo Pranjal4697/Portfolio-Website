c2ff5ada9a3c73dd85f78ab673ff1be9d22ac8c4
pranjal4697 <pranjal4697@gmail.com>
2025-02-25 20:53:31 +0530

    Revamped the website and resolved security warning

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"8oBQw1hujJOvxwBnwD8M8bMVxmXPZll9itOFTB5sYvc=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
